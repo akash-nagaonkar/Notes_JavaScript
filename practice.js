50f0237ab28b646f4d28984865415c7aa55b790c
Akash Nagaonkar <akash.nagaonkar@infosys.com>
2023-09-08 13:09:34 +0530

    Completed

---
+++ practice.js
@@ -0,0 +1,51 @@
+const box = document.querySelector(".center");
+const colorText = document.querySelector(".text");
+
+box.addEventListener("mousemove", (e) => {
+  const boxLocation = box.getBoundingClientRect();
+  const insideBoxLocation = e.clientX - boxLocation.left;
+
+  if (insideBoxLocation < boxLocation.width / 2 - 5) {
+    colorText.innerText = "Red";
+    let redColor = gsap.utils.mapRange(
+      0,
+      boxLocation.width / 2,
+      255,
+      0,
+      insideBoxLocation
+    );
+
+    gsap.to(box, {
+      backgroundColor: `rgb(${redColor}, 0, 0)`,
+      color: "black",
+    });
+  } else if (insideBoxLocation > boxLocation.width / 2 + 5) {
+    colorText.innerText = "Blue";
+    let redColor = gsap.utils.mapRange(
+      boxLocation.width / 2,
+      boxLocation.width,
+      255,
+      0,
+      insideBoxLocation
+    );
+
+    gsap.to(box, {
+      backgroundColor: `rgb(0, 0, ${redColor})`,
+      color: "black",
+    });
+  } else {
+    colorText.innerText = "Black";
+    gsap.to(box, {
+      backgroundColor: "black",
+      color: "white",
+    });
+  }
+});
+
+box.addEventListener("mouseleave", ()=>{
+  colorText.innerText = "Green"
+  gsap.to(box, {
+    backgroundColor:"green",
+    color:"white"
+  })
+})
